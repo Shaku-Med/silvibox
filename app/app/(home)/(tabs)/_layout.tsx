071efda68f7c0c689a12bf7400dd6f7f441b1714
Shaku-Med <jujubelt124@gmail.com>
2025-02-28 18:32:46 -0500

    s

---
+++ app/app/(home)/(tabs)/_layout.tsx
@@ -0,0 +1,22 @@
+// app/_layout.js
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+export default function RootLayout() {
+  return (
+    <>
+      <StatusBar style="light" backgroundColor="#6200EE" />
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#6200EE',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+    </>
+  );
+}
--- app/app/(home)/_layout.tsx
+++ app/app/(home)/_layout.tsx
@@ -1,21 +1,17 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Drawer } from 'expo-router/drawer';
 import { useEffect } from 'react';
-// import * as ScreenOrientation from 'expo-screen-orientation';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 export default function Layout() {
-  // useEffect(() => {
-  //   ScreenOrientation.Orientation.LANDSCAPE_LEFT
-  // })
+  useEffect(() => {
+    ScreenOrientation.Orientation.LANDSCAPE_LEFT
+  })
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer>
+      <Drawer screenOptions={{ headerShown: false }}>
         <Drawer.Screen
-          name="index"
-          options={{
-            drawerLabel: ``,
-            title: 'overview',
-          }}
+          name="(tabs)"
         />
       </Drawer>
     </GestureHandlerRootView>
